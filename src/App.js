c28bc9db315278b350234d5e1d7e43328a13200e
Namhotis <mathis.dyk.pro@gmail.com>
2021-06-19 21:04:40 +0200

    Init React app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Bienvenue sur mon application React utilisant Firebase. Maintenant, il ne reste plus qu'à coder !
+    </div>
+  );
+}
+
+export default App;
